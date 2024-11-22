2d29c6b997612fd03287580e5c18d24c631fc0a2
elfaiza <efje.021@gmail.com>
2024-11-22 15:09:52 +0800

    Tugas Module 4 & 6

---
+++ dizablewindow.py
@@ -0,0 +1,32 @@
+from PyQt5.QtWidgets import QMainWindow, QWidget, QApplication, QPushButton, QLabel, QDesktopWidget
+from PyQt5 import QtCore
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.label = QLabel(self)
+        self.label.setText("Label1")
+        self.label.move(200, 0)
+        
+        self.button = QPushButton(self)
+        self.button.setText("Button1")
+        
+        self.setGeometry(0, 0, 500, 500)
+        self.setWindowTitle("Belajar PyQt5")
+        
+        # Cek ukuran main window app kita
+        cwa = self.frameGeometry()
+        cwc = QDesktopWidget().availableGeometry().center()  # Pada screen saya: (682, 363)
+        cwa.moveCenter(cwc)
+        self.move(cwa.topLeft())
+
+        # Agar tidak bisa di-resize
+        self.setFixedSize(500, 500)
+        
+        # Menghilangkan border window
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+
+app = QApplication([])
+window = MyWindow()
+window.show()
+app.exec_()
